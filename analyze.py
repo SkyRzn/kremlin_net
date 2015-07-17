e4bfadd763e5d9f8e407731ea07a5c7ac033ec8c
Alexandr Ivanov <alexandr.sky@gmail.com>
2015-07-17 18:24:00 +0300

    Улучшил отображение графа.

--- analyze.py
+++ analyze.py
@@ -3,6 +3,7 @@
 
 
 import json
+import manual_data
 
 
 def analyze():
@@ -15,21 +16,33 @@ def analyze():
 		return
 
 	data = json.loads(j)
+	manual_data.add_manual_data(data)
+
+	for k, v in data.items():
+		if 'ga' in v:
+			ga = v['ga']
+			for ua in ga:
+				ua = ua[:11]
+				if ua not in manual_data.checked:
+					print ua
+
 
 	ga = {}
 	ym = {}
 	res = {}
 	for k, v in data.items():
-		for kk, vv in v['whois']:
-			if ('privacyprotect.org' not in vv) and \
-				('abuse@reg.ru' not in vv) and \
-				('abuse@godaddy.com' not in vv) and \
-				('abuse@regtime.net' not in vv) and \
-				('publicdomainregistry.com' not in vv) and \
-				('@' in vv):
-				print '!!!!!!!', k, kk, vv
-		#if v['whois'].get('e-mail'):
-			#print '!!!!!!!', v['whois'].get('e-mail')
+		if 'whois' in v:
+			for kk, vv in v['whois']:
+				if ('privacyprotect.org' not in vv) and \
+					('abuse@reg.ru' not in vv) and \
+					('abuse@godaddy.com' not in vv) and \
+					('abuse@regtime.net' not in vv) and \
+					('publicdomainregistry.com' not in vv) and \
+					('@' in vv):
+					print '!!!!!!!', k, kk, vv
+			#if v['whois'].get('e-mail'):
+				#print '!!!!!!!', v['whois'].get('e-mail')
+
 		if type(v) != dict:
 			continue
 
@@ -49,10 +62,10 @@ def analyze():
 			#res[ua].add(k)
 
 	for k, v in res.items():
-		if len(v) > 0:
+		if len(v) > 1:
 			print k
-			#for url in v:
-				#print '\t', url
+			for url in v:
+				print '\t', url
 
 analyze()
 
--- graph.py
+++ graph.py
@@ -9,6 +9,20 @@ from manual_data import add_manual_data
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+def id_count(id_, data):
+	n = 0
+	for url, info in data.items():
+		if id_ in info.get('ga', []):
+			n += 1
+		if id_ in info.get('ym', []):
+			n += 1
+	return n
+
+def cut_ga(data):
+	for url, info in data.items():
+		if info.get('ga'):
+			info['ga'] = [v[:11] for v in info['ga']]
+
 
 def draw():
 	f = open('data.json', 'r')
@@ -23,12 +37,16 @@ def draw():
 
 	add_manual_data(data)
 
+	cut_ga(data)
+
 	graph = nx.Graph()
 
 	nodes = {}
 	edges = []
 
-	acc_pix = QPixmap('acc.png')
+	ga_pix = QPixmap('google.png')
+	ym_pix = QPixmap('yandex.png')
+	email_pix = QPixmap('email.png')
 	site_pix = QPixmap('site.png')
 
 	for url, info in data.items():
@@ -36,18 +54,32 @@ def draw():
 			continue
 		for id_ in info.get('ga', []):
 			id_ = id_[:11]
-			graph.add_edge(url, id_)
-			edges.append([url, id_])
-			nodes[id_] = QGraphicsPixmapItem(acc_pix)
-			nodes[id_].name = id_
+			if id_count(id_, data) > 1:
+				graph.add_edge(url, id_)
+				edges.append([url, id_])
+				nodes[id_] = QGraphicsPixmapItem(ga_pix)
+				nodes[id_].name = id_
+				nodes[id_].setFlag(QGraphicsItem.ItemIsMovable, True)
 		for id_ in info.get('ym', []):
 			id_ = id_[:11]
-			graph.add_edge(url, id_)
-			edges.append([url, id_])
-			nodes[id_] = QGraphicsPixmapItem(acc_pix)
-			nodes[id_].name = id_
+			if id_count(id_, data) > 1:
+				graph.add_edge(url, id_)
+				edges.append([url, id_])
+				nodes[id_] = QGraphicsPixmapItem(ym_pix)
+				nodes[id_].name = id_
+				nodes[id_].setFlag(QGraphicsItem.ItemIsMovable, True)
+
+		if info.get('email'):
+			email = info.get('email')
+			nodes[email] = QGraphicsPixmapItem(email_pix)
+			nodes[email].name = email
+			nodes[email].setFlag(QGraphicsItem.ItemIsMovable, True)
+			graph.add_edge(url, email)
+			edges.append([url, email])
+
 		nodes[url] = QGraphicsPixmapItem(site_pix)
 		nodes[url].name = url
+		nodes[url].setFlag(QGraphicsItem.ItemIsMovable, True)
 		graph.add_node(url)
 
 	for key, data in nx.graphviz_layout(graph).items():
@@ -60,8 +92,6 @@ def draw():
 	return nodes, edges
 
 
-
-
 app = QApplication(sys.argv)
 win = QGraphicsView()
 
@@ -70,19 +100,37 @@ win.setScene(scene)
 
 nodes, edges = draw()
 
-for n1, n2 in edges:
-	p1 = nodes[n1].pos()
-	p2 = nodes[n2].pos()
-	p1.setX(p1.x() + 16)
-	p1.setY(p1.y() + 16)
-	p2.setX(p2.x() + 16)
-	p2.setY(p2.y() + 16)
-	scene.addLine(QLineF(p1, p2))
 
-for node in nodes.values():
-	scene.addItem(node)
-	it = scene.addText(node.name)
-	it.setPos(node.pos())
+
+def redraw(self):
+	for item in scene.items():
+		if type(item) == QGraphicsLineItem or type(item) == QGraphicsTextItem or type(item) == QGraphicsPixmapItem:
+			scene.removeItem(item)
+
+	for n1, n2 in edges:
+		p1 = nodes[n1].pos()
+		p2 = nodes[n2].pos()
+		p1.setX(p1.x() + 16)
+		p1.setY(p1.y() + 16)
+		p2.setX(p2.x() + 16)
+		p2.setY(p2.y() + 16)
+		scene.addLine(QLineF(p1, p2))
+
+	for node in nodes.values():
+		scene.addItem(node)
+
+	for node in nodes.values():
+		it = scene.addText(node.name)
+		it.name = node.name
+		pos = QPointF(node.pos())
+		pos.setX(pos.x() - it.boundingRect().width()/2 + 16)
+		pos.setY(pos.y() + 24)
+		it.setPos(pos)
+
+
+
+win.mouseDoubleClickEvent = redraw
+#redraw()
 
 win.setGeometry(QRect(100, 100, 400, 400))
 win.show()
